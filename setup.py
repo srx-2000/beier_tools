37bf7e5125574021ca22e9c53f8e55f5d548ea71
shirongxing <shi.rongxing@trs.com.cn>
2023-10-11 17:20:00 +0800

    JsOperation

---
+++ setup.py
@@ -0,0 +1,13 @@
+# setup.py文件内容
+from setuptools import setup
+
+setup(
+    name='beierTools',  # 应用名（pip安装和卸载时的名字）
+    version='0.0.6',  # 当前版本
+    author='Beier',  # 作者
+    author_email='1601684622@qq.com',  # 作者邮箱
+    licence='MIT License',  # 许可协议
+    url='https://github.com/srx-2000/beier_tools',  # 应用主页链接
+    description='Beier developments tools',
+    install_requires=[],  # 依赖包
+)
